940c548557e321e6af33c6cc0412982cc29a73d9
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-09-12 15:35:45 +0200

    Add files via upload
     Add src folder with homepage files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to yNot MVP</h1>
+      <p>This is your homepage. Everything is working!</p>
+    </div>
+  );
+}
+
+export default App;
