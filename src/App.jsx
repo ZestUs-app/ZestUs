5406cc4b14d7e62e67c61cc61d944acc58ba92df
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-09-16 17:06:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import React from 'react';
-import HomeFeed from './components/HomeFeed';
-import DiscoverProfessions from './components/DiscoverProfessions';
-import ProjectsRewards from './components/ProjectsRewards';
-import BalloonChat from './components/BalloonChat';
-import TeslaTutor from './components/TeslaTutor';
-import FunButton from './components/FunButton';
-import GlobalChallenges from './components/GlobalChallenges';
-import CoolCompanies from './components/CoolCompanies';
-import InvestorsCorner from './components/InvestorsCorner';
-import VisitorCounter from './components/VisitorCounter';
-import AISafetyTools from './components/AISafetyTools';
-
-function App() {
+export default function App() {
   return (
-    <div className="app">
-      <HomeFeed />
-      <DiscoverProfessions />
-      <ProjectsRewards />
-      <BalloonChat />
-      <TeslaTutor />
-      <FunButton />
-      <GlobalChallenges />
-      <CoolCompanies />
-      <InvestorsCorner />
-      <VisitorCounter />
-      <AISafetyTools />
+    <div>
+      <h1>Welcome to yNot MVP</h1>
     </div>
   );
 }
-
-export default App;
