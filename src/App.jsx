04244e6543148f8da6a573f35e584721164c55e6
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-09-17 09:23:48 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to yNot MVP</h1>
+      <p>This is a working React + Vite app deployed on Netlify.</p>
+    </div>
+  );
+}
