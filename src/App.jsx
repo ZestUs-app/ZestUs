eae6b3d0b582758533931aa2c400827087cbb9a2
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-09-19 16:34:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import Contact from './components/Contact/Contact';
 import VisitorCounter from './components/VisitorCounter/VisitorCounter';
-
-import React from 'react';
 
 export default function App() {
   return (
     <div style={{ textAlign: 'center', padding: '2rem' }}>
-      <h1>Welcome to yNot MVP</h1>
+      <h1>Welcome to ZestUs</h1>
       <p>This is a working React + Vite app deployed on Netlify.</p>
+
+      {/* Visitor Counter */}
+      <VisitorCounter />
+
+      {/* Contact Form */}
+      <Contact />
     </div>
   );
 }
