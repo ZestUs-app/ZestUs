8bbe4d0aa17f0d82718db9796da476d3039afa8b
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-09-23 16:36:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ZestUs/',
+  base: '/ZestUs/', // 👈 MUST match your GitHub repo name
   plugins: [react()],
-});
