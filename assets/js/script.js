5244a73339bf9fd874eddd86169cbe86cf6bc17d
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-10-03 17:44:38 +0200

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    const visitorCounter = document.getElementById("visitor-counter");
-    visitorCounter.textContent = "Visitors: 123 | Location: Guest | Age Group: Guest";
-});
