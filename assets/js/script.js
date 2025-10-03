002411aa56c75884df62d1050e0530fe08b7a0cb
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-10-03 18:42:53 +0200

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('darkModeToggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
-
-function openChat(message) {
-    alert("Balloon says: " + message);
-}
