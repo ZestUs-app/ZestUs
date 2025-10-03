9ed5aa0cc5737f3cb686a3ddff0c81daa22cafcb
zeljkapotoku-a11y <zeljka.potoku@gmail.com>
2025-10-03 17:08:14 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate visitor counter
+    document.getElementById("visitor-counter").textContent = "Visitors: 123 | Location: Croatia | Age: Teen";
+});
